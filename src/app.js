79098c61899d3d0c43048415fd734b62087f9e76
Priyanshu Das <dasp98458@gmail.com>
2026-01-21 06:31:36 +0530

    Implemented app.js and server.js

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const index = require("../src/routes/index");
+const errorHandler = require("../src/middlewares/errorHandler");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Main endpoint from which all the types of api's will be called
+app.use("/api/v1", index);
+
+// Error handler middleware
+app.use(errorHandler);
+
+module.exports = app;
