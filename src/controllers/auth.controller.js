c43934bdccdcae396279fe8110d2fb924e1a6912
Priyanshu Das <dasp98458@gmail.com>
2026-01-24 00:48:50 +0530

    Implemented auth controller and auth routes

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,60 @@
+const registerUser = require("../services/auth.service");
+const loginUser = require("../services/auth.service");
+const generateAccessToken = require("../utils/jwt");
+const generateRefreshToken = require("../utils/jwt");
+
+exports.register = async (req, res, next) => {
+  try {
+    const { name, email, password, role, org_id } = req.body;
+
+    const newUser = await registerUser(name, email, password, role, org_id);
+
+    res.status(201).json({
+      success: true,
+      newUser,
+      message: "User registered successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const currUser = await loginUser(email, password);
+
+    const accessToken = generateAccessToken(currUser);
+    const refreshToken = generateRefreshToken(currUser);
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      sameSite: "none",
+      secure: true,
+    });
+
+    res.status(200).json({
+      success: true,
+      accessToken,
+      message: "User logged in successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.logout = async (req, res, next) => {
+  try {
+    res.clearCookie("refreshToken");
+
+    res.status(200).json({
+      success: true,
+      message: "User logged out successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,11 @@
+const register = require("../controllers/auth.controller");
+const login = require("../controllers/auth.controller");
+const logout = require("../controllers/auth.controller");
+
+const router = require("express").Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
+
+module.exports = router;
