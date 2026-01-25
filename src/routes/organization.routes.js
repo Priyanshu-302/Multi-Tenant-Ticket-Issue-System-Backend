f297f2ecab74c76a6367a9a9816637c424a5985f
Priyanshu Das <dasp98458@gmail.com>
2026-01-25 13:10:00 +0530

    Implemented ticket routes

--- src/routes/organization.routes.js
+++ src/routes/organization.routes.js
@@ -2,12 +2,13 @@ const createOrganization = require("../controllers/organization.controller");
 const addMember = require("../controllers/organization.controller");
 const getUserOrganizations = require("../controllers/organization.controller");
 const changeUserRole = require("../controllers/organization.controller");
+const roleHandler = require("../middlewares/role.middleware");
 
 const router = require("express").Router();
 
 router.post("/create", createOrganization);
-router.post("/add-member", addMember);
+router.post("/add-member", roleHandler('ADMIN'), addMember);
 router.post("/get-user-orgs", getUserOrganizations);
-router.post("/change-user-role", changeUserRole);
+router.post("/change-user-role", roleHandler('ADMIN'), changeUserRole);
 
 module.exports = router;
